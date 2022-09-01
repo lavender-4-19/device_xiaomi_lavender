b8756b0160d3eb985e0ee8e76d0643e5a8a2ade7
ImPrashantt <prashant33968@gmail.com>
2022-09-01 17:26:03 +0530

    lavender: Superior bringup
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_lavender.mk
+    $(LOCAL_DIR)/superior_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_lavender-user \
-    arrow_lavender-userdebug \
-    arrow_lavender-eng
+    superior_lavender-user \
+    superior_lavender-userdebug \
+    superior_lavender-eng
--- superior_lavender.mk
+++ superior_lavender.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common SuperiorOS stuff
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -17,10 +17,8 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := arrow_lavender
+PRODUCT_NAME := superior_lavender
 PRODUCT_MODEL := Redmi Note 7
-
-DEVICE_MAINTAINER := Ratoriku, Hazama25
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
